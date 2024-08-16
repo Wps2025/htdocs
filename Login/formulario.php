b7eebd63fa29c2316b78af61ddc54451ac8e9616
Wps2025 <140184633+Wps2025@users.noreply.github.com>
2024-08-16 11:35:06 -0300

    refactor.alter formulario.php

--- Login/formulario.php
+++ Login/formulario.php
@@ -1,13 +1,21 @@
 <?php
 
-    if(isset($_POST['submit']))
-    {
-        print_r($_POST['nome']);
-        print_r('<br>');
-        print_r($_POST['email']);
-        print_r('<br>');
-        print_r($_POST['senha']);
-    }
+    //if(isset($_POST['submit']))
+    // {
+    //print_r($_POST['nome']);
+    //print_r('<br>');
+    //  print_r($_POST['email']);
+    //print_r('<br>');
+    //print_r($_POST['senha']);
+    //}
+
+include_once('config.php');
+
+$nome = $_POST["nome"];
+$email = $_POST["email"];
+$senha = $_POST["senha"];
+
+$result = mysqli_query($conexao,"INSERT INTO usuarios(nome,email,senha)VALUES ('$nome','$email','$senha')");
 
 ?>
 
