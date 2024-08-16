6de0a3fcf7c2e4b863f3d5bacd531413f19dcb58
Wps2025 <140184633+Wps2025@users.noreply.github.com>
2024-08-16 10:29:34 -0300

    refactor.alter formulario.php

--- Login/formulario.php
+++ Login/formulario.php
@@ -17,7 +17,86 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login do candidato/cadastro</title>
-    <link rel="stylesheet" href="formulario.css">
+<style>
+    body{
+    font-family: Arial, Helvetica, sans-serif;
+    color: black;
+    background-image: linear-gradient(45deg, red, yellow);
+}
+.box{
+    background-image:linear-gradient(45deg, yellow, red);
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    padding: 60px;
+    border-radius: 15px;
+}
+a{
+    color: black;
+    text-decoration: none;
+    background-image: linear-gradient(40deg, yellow, red);
+    padding: 15px;
+    border-radius: 13px;
+    cursor: pointer;
+    border-bottom: 5px solid black;
+}
+input{
+    padding: 15px;
+    border: none;
+    outline: none;
+    font-size: 15px;
+    width: 90%;
+}
+.inputBox{
+    position: relative;
+}
+.inputUser{
+    background: none;
+    border: none;
+    border-bottom: 1px solid black;
+    outline: none;
+    color: black;
+    font-size: 15px;
+}
+.labelInput{
+    position: absolute;
+    top: 0px;
+    left: 0px;
+    pointer-events: none;
+    transition: .5px;
+}
+.inputUser:focus~.labelInput,
+.inputUser:valid~.labelInput{
+    top: -20px;
+    font-size: 15px;
+    padding: 5px;
+    
+}
+fieldset{
+    border: 2px solid rgba(206, 81, 23, 0.952);
+    border-radius: 5px;
+}
+legend{
+    border: 2px solid rgba(206, 81, 23, 0.952);
+    padding: 5px;
+    text-align: center;
+    border-radius: 5px;
+    color: black;
+}
+#submit{
+    background-image:linear-gradient(50deg, red, yellow);
+    border-radius: 5px;
+    cursor:pointer;
+    border: none;
+    padding: 15px;
+    width: 100%;
+    font-size: 15px;
+}
+#submit:hover{
+    background-image:linear-gradient(50deg, yellow, red);
+}
+</style>
 </head>
 <body>
     <a href="login.html">Volta</a>
--- Login/home.php
+++ Login/home.php
@@ -4,7 +4,30 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>SITE | HOME</title>
-    <link rel="stylesheet" href="home.css">
+    <style>
+        body{
+    font-family: Arial, Helvetica, sans-serif;
+    background-image: linear-gradient(45deg, red, yellow);
+    color: black;
+}
+div{
+    color: black;
+    text-decoration: none;
+    position: absolute;
+    top: 30%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    padding: 60px;
+}
+h1{
+    text-align: center;
+}
+a{
+    color: black;
+    text-decoration: none;
+    font-size: 30px;
+}
+    </style>
 </head>
 <body>
     <h1>Sejam bem-vindos</h1>
