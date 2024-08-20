e2f2b82fd769d0113a9cbe7319ccaabd6fcdd1a6
Wps2025 <140184633+Wps2025@users.noreply.github.com>
2024-08-20 09:08:52 -0300

    refactor.alter

---
+++ Login/theory.php
@@ -0,0 +1,13 @@
+<?php
+    session_start();
+    session_destroy();
+
+if((!isset($_SESSION['email']) == true ) and (!isset($_SESSION['senha']) == true))
+{
+    unset($_SESSION['email']);
+    unset($_SESSION['senha']);
+    
+    header('Location: theory.php');
+}
+
+?>
