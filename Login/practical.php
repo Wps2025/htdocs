7348188c9ec7b01d0d72518bfe221c1afe13ba30
Wps2025 <140184633+Wps2025@users.noreply.github.com>
2024-08-20 09:29:40 -0300

    refacter.alter

--- Login/practical.php
+++ Login/practical.php
@@ -1,11 +1,15 @@
 <?php
     session_start();
     session_destroy();
+
 if((!isset($_SESSION['email']) == true ) and (!isset($_SESSION['senha']) == true))
 {
     unset($_SESSION['email']);
     unset($_SESSION['senha']);
+
     header('Location: practical.php');
+    header('Location: login.php');
+    
 }
 
 ?>
--- Login/theory.php
+++ Login/theory.php
@@ -6,8 +6,9 @@ if((!isset($_SESSION['email']) == true ) and (!isset($_SESSION['senha']) == true
 {
     unset($_SESSION['email']);
     unset($_SESSION['senha']);
-    
+
     header('Location: theory.php');
+    header('Location: login.php');
 }
 
 ?>
