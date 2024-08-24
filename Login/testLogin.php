b96620bf919cfb5c9937e473e747e4054702a5ab
Wps2025 <140184633+Wps2025@users.noreply.github.com>
2024-08-24 10:45:44 -0300

    refactor.alter

--- Login/testLogin.php
+++ Login/testLogin.php
@@ -5,45 +5,44 @@ session_start();
 //print_r($_REQUEST); São os parâmentro que estão vindo,
 // do formulario do meu login.
 
-if (isset($_POST['submit']) && !empty($_POST['email']) && !empty($_POST['senha'])) {
+    if (isset($_POST['submit']) && !empty($_POST['email']) && !empty($_POST['senha'])) {
 
-    //Vai deixar acessa o sistema.
-    include_once('config.php');
+            //Vai deixar acessa o sistema.
+            include_once('config.php');
 
-    $email = $_POST['email'];
-    $senha = $_POST['senha'];
-    $senha_hash = password_hash($senha, PASSWORD_DEFAULT);
+            $email = $_POST['email'];
+            $senha = $_POST['senha'];
+            $senha_hash = password_hash($senha, PASSWORD_DEFAULT);
 
+                //print_r('Email: ' . $email);|//Testando as minhas variáveis
+                //print_r('<br>');            |//email e senha que está chegando
+                //print_r('Senha: ' . $senha);|//os dados.
 
+                //Sistema de sugurança:Comparação do email e senha no banco de dados no Mysqli.
+            $sql =  "SELECT * FROM usuarios WHERE email = '$email' and senha = '$senha'";
 
-    //print_r('Email: ' . $email);|//Testando as minhas variáveis
-    //print_r('<br>');            |//email e senha que está chegando
-    //print_r('Senha: ' . $senha);|//os dados.
+            $result = $conexao->query($sql);
 
-    //Sistema de sugurança:Comparação do email e senha no banco de dados no Mysqli.
-    $sql =  "SELECT * FROM usuarios WHERE email = '$email' and senha = '$senha'";
+            //print_r($sql);
+            //print_r($result);
 
-    $result = $conexao->query($sql);
+            //Irá verificar quantidades de linhas se for menor de 1 "Não existe"
+            //| Mas se for maior do que 0 "Existe".
+            if (mysqli_num_rows($result) < 1) {
 
-    //print_r($sql);
-    //print_r($result);
+            unset($_SESSION['email']); // Caso não existe email e senha, será
+            unset($_SESSION['senha']); // | destruido.
+            header('Location: login.php');
+            } else {
 
-    //Irá verificar quantidades de linhas se for menor de 1 "Não existe"
-    //| Mas se for maior do que 0 "Existe".
-    if (mysqli_num_rows($result) < 1) {
-
-        unset($_SESSION['email']); // Caso não existe email e senha, será
-        unset($_SESSION['senha']); // | destruido.
-        header('Location: login.php');
-    } else {
-
-        $_SESSION['email'] = $email; // Caso existir o email e senha,
-        $_SESSION['senha'] = $senha; // terá acesso.
-        header('Location: sistemaS.php');
-    }
+            $_SESSION['email'] = $email; // Caso existir o email e senha,
+            $_SESSION['senha'] = $senha; // terá acesso.
+            header('Location: sistemaS.php');
+        }
 } else {
 
-    header('location: login.php'); //Caso não existir uma variavel,
-    //não vai deixar acessa o sistema. Irá retornar para o sistema login.
-
+        header('location: login.php'); //Caso não existir uma variavel,
+         //não vai deixar acessa o sistema. Irá retornar para o sistema login.
 }
+
+?>
