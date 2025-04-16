0e275cc1703792de64b0409d058ecff84f8cfda9
william <wps201418@gmai.com>
2025-04-16 06:28:04 -0300

    refactor: atualizar URL para produção nos arquivos .env e configUrl.php

--- config/configUrl.php
+++ config/configUrl.php
@@ -1,9 +1,7 @@
 <?php
 
 if (!defined('URL')) {
-<<<<<<< HEAD
-    define('URL', 'https://projeto-candidatos.shop/');
-=======
-    define('URL',https://projeto-candidatos.netlify.app/');
->>>>>>> 4e51f686eea0793f9d80db678355b3b1a9899fcb
+    define('URL', 'https://projeto-candidatos.shop/structure/');
 }
+
+?>
