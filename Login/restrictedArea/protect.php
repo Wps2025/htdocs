7ded15839b1657b2178aa77cfe2f05b128e3a48e
william <wps201418@gmai.com>
2025-04-16 17:27:52 -0300

    refactor: redirecionar usuários não autenticados para página de erro em vez de exibir mensagem

--- Login/restrictedArea/protect.php
+++ Login/restrictedArea/protect.php
@@ -14,9 +14,7 @@ if (session_status() === PHP_SESSION_NONE) {
 }
 if (!isset($_SESSION['user_id'])) {
     session_destroy();
-    echo nl2br(htmlspecialchars("Não será possível acessar o sistema! Cadastre-se no formulário ou acesse o login novamente!"));
-    echo "<br><a>Não tem uma conta? </a><a href='/projeto-candidatos/structure/formulario'>Cadastre-se</a>";
-    echo "<a> ou </a><a href='/projeto-candidatos/structure/login'>Login</a>";
-    exit();
+    header('Location: /projeto-candidatos/structure/error?restricted');
+    exit;
 
 }
