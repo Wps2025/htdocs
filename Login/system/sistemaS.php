d634f162a7f4069e4a191920b676837c75415759
william <wps201418@gmai.com>
2025-04-26 20:12:00 -0300

    Atualizar

--- Login/system/sistemaS.php
+++ Login/system/sistemaS.php
@@ -11,7 +11,7 @@ include_once '../restrictedArea/protect.php';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sistema | logado </title>
-    <link rel = "stylesheet" type="text/css" href="../system/css/sistemaS.css">
+    <link rel = "stylesheet" type="text/css" href="/htdocs/login/system/css/sistemaS.css">
 </head>
 <body>
     <header>
